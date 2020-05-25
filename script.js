d850476e94c67032cc0cc3fe4c605a6840f6390f
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2020-05-25 07:17:32 +0100

    Second Commit: Create Classes and Subclasses

---
+++ script.js
@@ -0,0 +1,38 @@
+//Town Class
+class Town{
+  constructor(name, buildYear){
+    this.name = name;
+    this.buildYear = buildYear;
+  }
+}
+//Park class
+class Park extends Town {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear);
+    this.area = area;
+    this.numTrees = numTrees;
+  }
+  //Tree density method
+  treeDensity() {
+    const density = this.numTrees / this.area;
+  	console.log(`${this.name} has a tree density of ${density} trees per square km.`);
+  }
+}
+//Street class
+class Street extends Town {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear);
+    this.length = length;
+    this.length = size;
+  }
+  //Classify street method
+    classifyStreet(){
+      const classification = new Map();
+      classification.set(1, 'tiny');
+      classification.set(2, 'small');
+      classification.set(3, 'normal');
+      classification.set(4, 'big');
+      classification.set(5, 'huge');
+      console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`);
+    }
+}
